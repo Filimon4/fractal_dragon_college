62a1e678cb820a31484ad74529b8c1e45372e903
Filmon <98771837+Filimon4@users.noreply.github.com>
2024-03-11 17:03:10 +0300

    Add files via upload

---
+++ lab15/lab15.pas
@@ -0,0 +1,131 @@
+﻿program test;
+
+procedure task1();
+type
+  ptr_int = ^integer;
+
+var
+  i: integer;
+  i_prt: ptr_int;
+begin
+  i := 2;
+  i_prt := @i;
+  writeln('i: ', i, ' ', 'i_ptr: ' ,i_prt^);
+end;
+
+function TakeWord(F: Text):string;
+var c: char;
+begin
+  reset(F);
+  Result:= '';
+  c := ' ';
+  while not eof(f) and (c <= ' ')do
+    read(F, c);
+  while not eof(f) and (c > ' ') do begin
+    Result := Result + c;
+    read(F, c);
+  end;
+  CloseFile(F);
+  TakeWord := Result;
+end;
+
+procedure task2();
+var 
+  r: string; 
+  f: TextFile;
+const FILE_NAME = 'words.txt';
+begin
+  Assign(f, FILE_NAME);
+  r := TakeWord(f);
+  writeln(r.Length);
+end;
+
+procedure task3();
+type
+  PNode = ^Node;
+  Node = record
+    val: integer;
+    next: pointer;
+  end;
+var
+  Head: PNode;
+  NewNode: PNode;
+  pp: PNode;
+  i: integer;
+begin
+  New(Head);
+  Head^.val := 0;
+  Head^.next := nil;
+  
+  while i <= 10 do begin
+    pp := Head;
+    while pp^.next <> nil do
+      pp := pp^.next;
+    New(NewNode);
+    NewNode^.val := i;
+    NewNode^.next := nil;
+    pp^.next := NewNode;
+    i := i + 1;
+  end;
+  
+  pp := Head^.next;
+  while pp^.next <> nil do begin
+    writeln(pp^.val);
+    pp := pp^.next;
+  end;
+  
+  pp := Head^.next;
+  while pp^.next <> nil do begin
+    if pp^.val mod 2 = 0 then
+      writeln(pp^.val);
+    pp := pp^.next;
+  end;
+end;
+
+procedure task4();
+type
+  PNode = ^Node;
+  Node = record
+    val: integer;
+    next: pointer;
+  end;
+var
+  Head: PNode;
+  NewNode: PNode;
+  pp: PNode;
+  i, min, max: integer;
+begin
+  New(Head);
+  Head^.val := 0;
+  Head^.next := nil;
+  
+  i := 501;
+  while i <= 1000 do begin
+    pp := Head;
+    while pp^.next <> nil do begin
+      pp := pp^.next;
+    end;
+    New(NewNode);
+    NewNode^.val := (Abs(1000 - i) + Abs(1000 - (i*i)) + Abs(1000 - (i*i*i))) mod 500;
+    NewNode^.next := nil;
+    pp^.next := NewNode;
+    i := i + 1;
+  end;
+  
+  pp := Head^.next;
+  min := pp^.val;
+  max := pp^.val;
+  while pp^.next <> nil do begin
+    if pp^.val > max then
+      max := pp^.val;
+    if pp^.val < min then
+      min := pp^.val;
+    pp := pp^.next;
+  end;
+  writeln(min);
+  writeln(max);
+end;
+
+begin
+  task4();
+end.
---
+++ lab16/lab16.pas
@@ -0,0 +1,77 @@
+﻿program ReverseListUsingDeque;
+
+type
+  PNode = ^Node;
+  Node = record
+    Data: Integer;
+    Next, Prev: PNode;
+  end;
+
+type
+  Deque = record
+    Front, Rear: PNode;
+  end;
+
+procedure AddFront(var d: Deque; value: Integer);
+var
+  NewNode: PNode;
+begin
+  New(NewNode);
+  NewNode^.Data := value;
+  NewNode^.Next := d.Front;
+  NewNode^.Prev := nil;
+
+  if d.Front <> nil then
+    d.Front^.Prev := NewNode
+  else
+    d.Rear := NewNode;
+
+  d.Front := NewNode;
+end;
+
+function RemoveFront(var d: Deque): Integer;
+var
+  TempNode: PNode;
+begin
+  if d.Front <> nil then
+  begin
+    TempNode := d.Front;
+    RemoveFront := TempNode^.Data;
+    d.Front := TempNode^.Next;
+
+    if d.Front <> nil then
+      d.Front^.Prev := nil
+    else
+      d.Rear := nil;
+
+    Dispose(TempNode);
+  end;
+end;
+
+var
+  InputFile, OutputFile: Text;
+  Number: Integer;
+  d: Deque;
+
+begin
+  Assign(InputFile, 'lab15-5.txt');
+  Reset(InputFile);
+
+  Assign(OutputFile, 'lab15-4.txt');
+  Rewrite(OutputFile);
+
+  d.Front := nil;
+  d.Rear := nil;
+
+  while not eof(InputFile) do
+  begin
+    Readln(InputFile, Number);
+    AddFront(d, Number);
+  end;
+
+  while d.Front <> nil do
+    Writeln(OutputFile, RemoveFront(d));
+
+  Close(InputFile);
+  Close(OutputFile);
+end.
